c8a7c8313272e4e3de5e5773c7daa08a383b9785
sandos187 <sandos@mailbox.org>
2019-06-14 15:15:18 +0200

    changed way of posting messages to slack from deprecated way to recommended incomming webhook

--- README.md
+++ README.md
@@ -9,11 +9,14 @@ In order to use this script, you need to install some python dependencies, which
 After that, you have to modify your configuration file. Copy `spotislack.cfg.sample` to `spotislack.cfg` and start editing:
 
 ### Slack
-Visit https://api.slack.com/custom-integrations/legacy-tokens and get a token that allows you t post messages via commandline
+Visit https://api.slack.com/apps and create an app for this script. After that, you have to add an Incoming Webhook to this App.
+**IMPORTANT: this only has to be done once for every workspace. If another user of your workspace already created an app for this script, ask him for the incomming webhook url.**
 
-Define the Channel in which your spotify-messages should appear
+The Channel to which the message of this script is posted depends on the Incoming Webhook. At the moment only one Webhook is supported. Maybe this is changed in the future, so that you can set more than one webhook in the config
 
 You can define a color for the message border and last but not least an optional icon.
+
+You must set your username in the configfile
 
 ### Spotify
 Visit https://developer.spotify.com/dashboard/login to create your own app. This is necessary to get the needed credentials for reading your personal playlist details
@@ -24,7 +27,7 @@ On the Apps Homepage you can edit the details and add an "Redirect URI". This is
 During the Authorisation process, you will get an error message in your browser, that the URL is not reacheable, but this doesn't matter as long as you can see and copy that complete URL. You need to paste that one into your terminal for the authorisation to finish
 
 ## Usage
-After sich installation and configuration process, you just need to call the script from terminal, and it should post the current song to the defined channel.
+After the installation and configuration process, you just need to call the script from terminal, and it should post the current song to the defined channel.
 
 ## ATTENTION!
 This script is far from perfect, and at this moment just a little bit more than a proof-of-concept. For example there isn't any error- and exception-handling right now. If spotify isn't running while executing this script you just get a cryptic runtime error.
--- spotislack.py
+++ spotislack.py
@@ -14,10 +14,11 @@ spotipy_token = None
 
 def read_config():
     """ read configfile """
-    global slack_token
+    global slack_url
     global slack_channel
     global slack_color
     global slack_footer_icon
+    global slack_username
     global spotipy_client_id
     global spotipy_client_secret
     global spotipy_redirect_uri
@@ -25,10 +26,11 @@ def read_config():
     with open(os.path.join(sys.path[0], 'spotislack.cfg')) as conf:
         config = ConfigParser()
         config.read_file(conf)
-    slack_token = config['slack']['slack_token']
+    slack_url = config['slack']['slack_url']
     slack_channel = config['slack']['slack_channel']
     slack_color = config['slack']['slack_color']
     slack_footer_icon = config['slack']['slack_footer_icon']
+    slack_username = config['slack']['slack_username']
     spotipy_client_id = config['spotify']['spotipy_client_id']
     spotipy_client_secret = config['spotify']['spotipy_client_secret']
     spotipy_redirect_uri = config['spotify']['spotipy_redirect_uri']
@@ -36,15 +38,15 @@ def read_config():
 
 
 def get_current_song_from_spotify(spotipy_token):
-    if spotipy_token:
-        global artist
-        global songname
-        global songurl
-        global album
-        global artwork
-        global fallback
-        sp = spotipy.Spotify(auth=spotipy_token)
-        results = sp.current_user_playing_track()
+    global artist
+    global songname
+    global songurl
+    global album
+    global artwork
+    global fallback
+    sp = spotipy.Spotify(auth=spotipy_token)
+    results = sp.current_user_playing_track()
+    if results:
         artist = results['item']['album']['artists'][0]['name']
         songname = results['item']['name']
         songurl = results['item']['uri'].replace('spotify:track:', 'https://open.spotify.com/track/')
@@ -52,39 +54,36 @@ def get_current_song_from_spotify(spotipy_token):
         artwork = results['item']['album']['images'][0]['url']
         fallback = "np: {0} - {1}".format(artist, songname)
     else:
-        print("Can't get token for", spotipy_username)
+        print('is spotify running?')
 
 
-def send_message_to_slack(token, channel, color, footer_icon, artist, songname, songurl, album, artwork, fallback):
+def send_message_to_slack(slackurl, color, username, footer_icon, artist, songname, songurl, album, artwork, fallback):
     """ Makes use of Send API:
         https://developers.facebook.com/docs/messenger-platform/send-api-reference
     """
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {0}'.format(slack_token),
     }
     payload = {
-        "channel": channel,
-        "as_user": 'true',
         "attachments": [
             {
                 "fallback": fallback,
                 "color": color,
-                "author_name": artist,
+                "author_name": username + " is listening to",
                 "title": songname,
                 "title_link": songurl,
-                "text": album,
+                "text": "by " + artist,
                 "thumb_url": artwork,
                 "footer": "now playing",
                 "footer_icon": footer_icon,
             }
         ]
     }
-    url = 'https://slack.com/api/chat.postMessage'
+
+    url = slackurl
     response = requests.post(url, headers=headers,
                              data=json.dumps(payload))
     response.raise_for_status()
-    return response.json()
 
 
 def main():
@@ -94,11 +93,11 @@ def main():
         read_config()
         spotipy_token = util.prompt_for_user_token(spotipy_username, scope, spotipy_client_id, spotipy_client_secret, spotipy_redirect_uri)
         get_current_song_from_spotify(spotipy_token)
-        send_message_to_slack(slack_token, slack_channel, slack_color, slack_footer_icon, artist, songname, songurl, album, artwork, fallback)
+        send_message_to_slack(slack_url, slack_color, slack_username, slack_footer_icon, artist, songname, songurl, album, artwork, fallback)
 
 
     except:
-        print("Connection Failed")
+        print("Something went wrong")
 
 
 if __name__ == "__main__":
